4a4b6975f01bc2d8537d695070ea91d6c74942a4
paulo <paulo.o.medeiros@hotmail.com>
2025-03-07 21:53:08 -0300

    feat: adicionar a classe ContaTerminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("BEM-VINDO À SIMULAÇÃO DE CONTA BANCÁRIA VIA TERMINAL");
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String numeroAgencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da conta!");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do titular da conta!");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta!");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente +
+                ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia +
+                ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
